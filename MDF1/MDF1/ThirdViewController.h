0023e5acc7012c4da40a237861b1f0c3d824c99c
Scott Caruso <scottcaruso@fullsail.edu>
2013-02-10 23:24:39 -0500

    Hooked up dummy third view to Tab Controller.

---
+++ MDF1/MDF1/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  MDF1
+//
+//  Created by Scott Caruso on 2/10/13.
+//  Copyright (c) 2013 Scott Caruso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
---
+++ MDF1/MDF1/ThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThirdViewController.m
+//  MDF1
+//
+//  Created by Scott Caruso on 2/10/13.
+//  Copyright (c) 2013 Scott Caruso. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
