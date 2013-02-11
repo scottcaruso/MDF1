e756ea3db8f9256996508f527344fe07adc4145b
Scott Caruso <scottcaruso@fullsail.edu>
2013-02-11 00:45:51 -0500

    Basic table functionality added.

---
+++ MDF1_week2/MDF1_week2/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TableViewController.h
+//  MDF1_week2
+//
+//  Created by Scott Caruso on 2/11/13.
+//  Copyright (c) 2013 Scott Caruso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController <UITableViewDelegate>
+{
+    
+}
+
+@end
