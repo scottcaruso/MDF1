7c12902399aecc67eda2e8a0b10d1271ecc8f9fb
Scott Caruso <scottcaruso@fullsail.edu>
2013-02-18 18:24:57 -0500

    Hooked up map.

--- MDV1_week3/MDV1_week3/SecondViewController.h
+++ MDV1_week3/MDV1_week3/SecondViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface SecondViewController : UIViewController
+@interface SecondViewController : UIViewController <MKMapViewDelegate>
 
 @end
